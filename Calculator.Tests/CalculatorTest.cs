2834e3df37116b2f09ce3e36faf0aa02f766db58
karthick <karthick.chandra@gmail.com>
2017-01-17 20:49:20 -0800

    sample repo

---
+++ Calculator.Tests/CalculatorTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculator.Tests
+{
+    [TestFixture]
+    public class CalculatorTest
+
+    {
+
+        [Test]
+        public void ShouldAddTwoNumbers()
+        {
+            Calculator sut = new Calculator();
+
+            int expectedResult = sut.Add(7, 8);
+
+            Assert.That(expectedResult, Is.EqualTo(15));
+
+        }
+
+
+
+        [Test]
+        public void ShouldMulTwoNumbers()
+        {
+
+            ICalculator sut = new Calculator();
+
+            int expectedResult = sut.Mul(7, 8);
+
+            Assert.That(expectedResult, Is.EqualTo(56));
+
+        }
+
+
+
+    }
+}
